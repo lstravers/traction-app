29bbe853a5f0084b8aa456edfdf5991406538634
lstravers <stravers222@gmail.com>
2018-10-01 15:28:51 -0400

    added jquery and initialized yarn for javascript

--- app/app/client/packs/application.js
+++ app/app/client/packs/application.js
@@ -8,3 +8,9 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
+import 'semantic-ui-css';
+
+import jQuery from 'jquery';
+window.jQuery = jQuery;
+window.$ = jQuery;
