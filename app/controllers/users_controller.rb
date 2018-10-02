f8a629704921a3b3b7de51e99bc0fd7e13055f42
lstravers <stravers222@gmail.com>
2018-10-02 15:47:47 -0400

    uncommented all methods from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,38 +12,39 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     redirect_to new_session_path, notice: "Your username was created successfully. Please login."
-  #   else
-  #     render 'new'
-  #   end
-  # end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_session_path, notice: "Your account was created successfully. Please login with your email address."
+    else
+      render 'new'
+    end
+  end
 
-  # def update
-  #   @user = User.find(params[:id])
+  def update
+    @user = User.find(params[:id])
   
-  #   if @user.update(book_params)
-  #     redirect_to @user
-  #   else
-  #     render 'edit'
-  #   end
-  # end
+    if @user.update(book_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   
-  # def destroy
-  #   @user = User.find(params[:id])
-  #   @user.destroy
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
    
-  #   redirect_to user_path(@user)
-  # end
-  # private
-  # def user_params
-  #   params.require(:user).permit(:name, :username, :email, :password)
-  # end
+    redirect_to user_path(@user)
+  end
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
