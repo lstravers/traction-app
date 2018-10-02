b79d2daebdf19cc2db3450be2d06cdd541e05eab
Laura <laura.s.chavez@outlook.com>
2018-10-02 15:10:45 -0400

    Control v01

--- db/schema.rb
+++ db/schema.rb
@@ -27,4 +27,9 @@ ActiveRecord::Schema.define(version: 2018_10_02_153727) do
     t.string "serial_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
