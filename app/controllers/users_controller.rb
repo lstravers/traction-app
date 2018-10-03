3979b9a76f3a0f77d3a93d561f18d8112d64c860
lstravers <stravers222@gmail.com>
2018-10-03 11:33:59 -0400

    modified users controller, create new user not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authentication
-  #before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :verify_authentication, only: [:create]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
     @user = User.all
@@ -8,22 +8,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     render json: @user
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path, notice: "Your account was created successfully. Please login with your email address."
+      render json: @user, notice: "Your account was created successfully. Please login with your email address."
     else
-      render json: @user
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    @user = User.find(params[:id])
-  
     if @user.update(book_params)
       redirect_to @user
     else
@@ -32,7 +29,6 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
    
     redirect_to user_path(@user)
@@ -40,7 +36,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :password, :email, :phone, :county, :address1, :address2, :city, :state, :zip, :admin, :contact_type, :date_auth, :admin_auth)
+    params.permit(:first_name, :last_name, :password, :email, :phone, :county, :address1, :address2, :city, :state, :zip, :admin, :contact_type, :date_auth, :admin_auth)
   end
 
   def set_user
