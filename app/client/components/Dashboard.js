31d6c01d12cc58f37c56e7b2225ee5ce43f6bd14
jeanetteobr <jeanette.obr@gmail.com>
2018-10-04 21:52:42 -0400

    Add WelcomeView component to mount to Dashboard

--- app/client/components/Dashboard.js
+++ app/client/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'bulma/css/bulma.css'
-import { Button, Title } from 'bloomer'
+
+import WelcomeView from './WelcomeView'
 
 class Dashboard extends React.Component {
   constructor () {
@@ -23,11 +24,7 @@ class Dashboard extends React.Component {
     return (
       <div className='Dashboard'>
         <div className='main'>
-          <Title> Welcome, {currentUser}!</Title>
-          <p>Third fridays seventy alt-country jazz food truck rigsbee cupcakes sustainable world beer festival train, the state of things duke chapel partner carpe durham eagles liberty food truck. Parrish street duke park durham divas southpoint basketball, duke carpe durham science and math, 70 ninth street dur'm.</p>
-          <Button className='is-primary'>Scan QR Code</Button>
-          <p>OR</p>
-          <Button className='is-primary'>Input Kit Serial Codes</Button>
+          <WelcomeView currentUser={currentUser} />
         </div>
       </div>
     )
---
+++ app/client/components/WelcomeView.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'bulma/css/bulma.css'
+import { Title, Button } from 'bloomer'
+
+class WelcomeView extends React.Component {
+  render () {
+    let {currentUser} = this.props
+    return (
+      <div>
+        <Title> Welcome, {currentUser}!</Title>
+        <p>Third fridays seventy alt-country jazz food truck rigsbee cupcakes sustainable world beer festival train, the state of things duke chapel partner carpe durham eagles liberty food truck. Parrish street duke park durham divas southpoint basketball, duke carpe durham science and math, 70 ninth street dur'm.</p>
+        <Button className='is-primary'>Scan QR Code</Button>
+        <p>OR</p>
+        <Button className='is-primary'>Input Kit Serial Codes</Button>
+      </div>
+    )
+  }
+}
+
+export default WelcomeView
