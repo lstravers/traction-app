e9048c698da9f60c7cab844579055e4363f104d4
Joanna Davidson <joanna@shrikesong.com>
2018-10-06 00:37:40 -0400

    More fixes for Heroku related to Devise gem

--- app/controllers/registrations/unlocks_controller.rb
+++ app/controllers/registrations/unlocks_controller.rb
@@ -1,6 +1,6 @@
-frozen_string_literal: true
+# frozen_string_literal: true
 
-class [registrations]::UnlocksController < Devise::UnlocksController
+class UnlocksController < Devise::UnlocksController
   # GET /resource/unlock/new
   def new
     super
