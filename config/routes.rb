d89f72bcb2557e2c01944afcea64c46a80209cd3
Laura <laura.s.chavez@outlook.com>
2018-10-06 07:36:24 -0400

    modified routes to add devise functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'signin', to: 'devise/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: "home#index"
