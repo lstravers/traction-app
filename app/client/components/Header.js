32f98b538f7a05f02e612b787ce4827a0bca4650
jeanetteobr <jeanette.obr@gmail.com>
2018-10-08 09:06:53 -0400

    Add is-active to header menu-items

--- app/client/components/Header.js
+++ app/client/components/Header.js
@@ -23,13 +23,13 @@ class Header extends Component {
           <nav className='navbar' role='navigation' aria-label='main navigation'>
             <div className='navbar-brand'>
               <Title isSize={3} onClick={() => window.location.href = '/'}>Harm Reduction Tracker</Title>
-              <a role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' onClick={this.handleClick}>
+              <a role='button' className='navbar-burger burger is-active' aria-label='menu is-active' aria-expanded='false' onClick={this.handleClick}>
                 <span aria-hidden='true' />
                 <span aria-hidden='true' />
                 <span aria-hidden='true' />
               </a>
             </div>
-            <div id='navbar-main' className='navbar-menu'>
+            <div id='navbar-main' className='navbar-menu is-active'>
               <div className='navbar-start'>
                 <a className='navbar-item' href='#'>
     Home
@@ -37,24 +37,24 @@ class Header extends Component {
               </div>
             </div>
 
-            <div id='navbar-main' className='navbar-menu'>
-              <div className='navbar-start'>
+            <div id='navbar-main' className='navbar-menu is-active'>
+              <div>
                 <a className='navbar-item' href='#'>
     Scan QR Code
                 </a>
               </div>
             </div>
 
-            <div id='navbar-main' className='navbar-menu'>
-              <div className='navbar-start'>
+            <div id='navbar-main' className='navbar-menu is-active'>
+              <div>
                 <a className='navbar-item' href='#'>
     Input Kit Serial Codes
                 </a>
               </div>
             </div>
 
-            <div id='navbar-main' className='navbar-menu'>
-              <div className='navbar-start'>
+            <div id='navbar-main' className='navbar-menu is-active'>
+              <div className='navbar-end'>
                 <a className='navbar-item'>
     Logout
                 </a>
