8cc4e2c18eb9e98977a06b108be8ebfd4d519c3a
Joanna Davidson <joanna@shrikesong.com>
2018-10-09 08:56:25 -0400

    added routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || home_index_path
+    if current_user.admin?
+      request.env['omniauth.origin'] || stored_location_for(resource) || home_index_path
+    else
+      request.env['omniauth.origin'] || stored_location_for(resource) || home_admin_path
+    end
   end
  
   # protect_from_forgery with: :null_session 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update]
 
   def index
-    @users = User.all
+    if current_user.admin?
+      @users = User.all
+    else
+      redirect_to root_path, alert: "Access denied"
   end
 
   def show
