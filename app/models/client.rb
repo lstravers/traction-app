90e9a4d657754ae9a1a17fb9125a33ae043ed7ce
Joanna Davidson <joanna@shrikesong.com>
2018-10-09 07:32:11 -0400

    adding info for admin users

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,10 @@ class Client < ApplicationRecord
   has_many :inventories
   has_one :client_confidential, dependent: :destroy
   accepts_nested_attributes_for :client_confidential
+
+  include PgSearch
+  pg_search_scope :search_by_county, :against => [:county]
+  pg_search_scope :search_by_city, :against => [:city]
+  pg_search_scope :search_by_user, :against => [:user_id]
+  
 end
--- app/models/reversal.rb
+++ app/models/reversal.rb
@@ -1,2 +1,7 @@
 class Reversal < ApplicationRecord
+    include PgSearch
+    pg_search_scope :search_by_county, :against => [:county]
+    pg_search_scope :search_by_town, :against => [:town]
+    pg_search_scope :search_by_user, :against => [:user_id]
+    
 end
