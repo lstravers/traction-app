03a4eca5d82e08ff5f66ce5c0ee76b27e1a780b6
Tina <christinawilson6@gmail.com>
2018-10-09 15:57:14 -0400

    cleaned up code

--- app/client/components/WelcomeView.js
+++ app/client/components/WelcomeView.js
@@ -8,7 +8,7 @@ class WelcomeView extends React.Component {
     let { currentUser, setManualInput } = this.props
     return (
       <div>
-        {/* <Title> Welcome, {currentUser.first_name}!</Title> */}
+        <Title> Welcome, {currentUser.first_name}!</Title>
         <p>Thank you for volunteering! Please select one of the options below to proceed. </p>
         <Button className='is-primary' onClick={() => window.location.href = '/kitserials'}>Scan QR Code</Button>
         <Button className='is-primary' onClick={() => window.location.href = '/kitserials?status=manual'}>Input Kit Serial Codes</Button>
