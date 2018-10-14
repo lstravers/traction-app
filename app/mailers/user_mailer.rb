db109189b4d078587cfc6a2d0d05cd6901c624a1
Laura <laura.s.chavez@outlook.com>
2018-10-14 11:18:19 -0400

    adding url for site in welcome mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,8 @@ class UserMailer < ApplicationMailer
        
         @greeting = "Welcome to Traction Application! Thank you for your support and volunteering:
          Use this email address to login #{@user.email}. You will need to choose the option 'Forgot Password?' to
-         set your new password. Link to Traction Appliction"
+         set your new password. Link to 
+         Traction Appliction: "
         mail(
         to: @user.email,
         from: 'tractionapp@gmail.com',
